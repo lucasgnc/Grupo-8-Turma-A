70d691f612d3119112508917c9d11df2a7c0598b
lucasgnc <116317725+lucasgnc@users.noreply.github.com>
2022-11-11 17:52:06 -0300

    Jogo

---
+++ Jogo.java
@@ -0,0 +1,325 @@
+package com.mycompany.rpg;
+
+import java.util.Scanner;
+
+
+public class Jogo {
+     public static void main(String[] args){
+     Scanner entrada = new Scanner (System.in);
+
+        String name;
+
+        
+
+
+                System.out.println("Entre nessa jornada com Cleitin o humano e construa sua própria nação."); 
+                System.out.println("\n");
+                System.out.println("No ano 5000, a terra vivia o auge da tecnologia…Os humanos estavam no ápice de "
+
+                + "sua inteligência, \nporém cientistas malucos, com uma ideia de fundir a inteligência humana "
+
+                + "com animais ocasionaram \numa explosão nuclear na qual levou quase toda a raça humana a "
+
+                + "extinção…O plano por sua vez deu certo animais \nganharam inteligência, mas grande parte "
+
+                + "se revoltou e começaram a construir \nsua própria civilização a fim de tornar o mundo um só "
+
+                + "para sua raça.\n5 de novembro de 5000… Cleitin te encontrar em uma caverna \nVocê com medo procura pegar pedras para se defender");
+
+                System.out.println("\n");
+
+                System.out.println("Cleitin: Ei calma…Eu sou um humano também - ele tenta te acalmar ");  
+
+                System.out.println("\n");
+
+                System.out.println("Cleitin: Me diga qual é o seu nome?");
+                 name = entrada.next();
+
+                System.out.println("\n");
+
+                System.out.println("Usuário: Meu Nome é " + name);  
+
+                System.out.println("\n");
+
+                System.out.println("Cleitin olá " + name + "... estou em busca de um líder para reconstruir minha nação, você está aqui á quanto tempo?");
+
+                System.out.println("\n");
+
+                System.out.println(name + ": já não sei a quanto tempo estou aqui, vivo aqui sozinho noite após noite");
+
+                System.out.println("\n");
+
+                System.out.println("Cleitin: Venha comigo, não sei mas você parecer ter um espírito de líder… \nEstou tentando construir uma nação, mas a verdade é que não sei governar nada. "
+
+                        + "\nEntão me diz você aceita ser um líder?");
+
+                System.out.print("\n");
+
+               
+
+               
+
+                Scanner s = new Scanner(System.in);
+
+               System.out.println("Deseja Entrar Nessa Jornada? [sim/nao]");
+
+        String lt= s.nextLine();
+
+
+        if(lt.equals("nao")) {
+
+        System.out.println("Ele morreu!");
+
+        }else if(lt.equals("sim")) {
+
+        System.out.println("Tenho dúvidas sobre isso, mas acredito que seja melhor do que viver aqui sozinho.Vamos nessa!!");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin e Zezin começam a caminhar, Zezin logo percebe que Cleitin é um cara que não \npara de falar e ama fazer uma piada com as situaçõe…. após longos, longos,longos "
+
+                + "\nminutos andando, chegam a ao pequeno vilarejo que Cleitin está construindo");
+
+        System.out.println("\n");
+
+        System.out.println(name + ":Nossa pensei que eu iria governar algo grandioso…");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin:Calma, Calma  isso é só um holograma…Temos que deixar a cidade disfarçada, somos muito fracos para sofrer um ataque, não temos estrutura para isso");
+
+        System.out.println("\n");
+
+        System.out.println("Entendo, desculpa por me precipitar! ");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin entra primeiro, logo depois Zezin vai atrás dele. Zezin fica admirado com tamanha "
+
+                + "\nbeleza da cidade…Uma cidade de tamanho médio, construída em volta de duas grandes "
+
+                + "\nrochas ,com uma ponte ligando as duas rochas, logo abaixo na parte térrea  casas e "
+
+                + "\npequenos prédios ainda destruídos por conta da explosão, mas, com pessoas e crianças "
+
+                + "\nfelizes por ainda ter um lar… Essa foi a primeira visão de Zezin sobre a cidade");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Gostou né? lolololol… Bem vamos andando quero te mostrar seus aposentos e "
+
+                + "\nainda hoje te anunciar como novo líder de nossa nação");
+
+        System.out.println("\n");
+
+        System.out.println(name +":Confesso que estou um pouco animado, você disse que sou o escolhido, mas tenho "
+
+                + "\nas minhas dúvidas…Talvez o povo nem me aceite");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Vamos, seja um pouco otimista vai dar bom!");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin leva Zezin para seus aposentos");
+
+        System.out.println("\n");
+
+        System.out.println(name  + ":onde fica meus aposentos?");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Nas Rochas");
+
+        System.out.println("\n");
+
+       System.out.println(name + ":orra, to importante lololololol. Tem elevador né?");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin:Não");
+
+        System.out.println("\n");
+
+        System.out.println(name + ":a para de brincadeira");
+
+        System.out.println("\n");
+
+        System.out.println(name + "a para de brincadeira");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: não é brincadeira");
+
+        System.out.println("\n");
+
+        System.out.println(name +" Pensando bem posso morar por aqui embaixo mesmo, pra que esse exagero né… Sempre gostei de voar baixo");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Você é muito preguiçoso, é claro que tem elevador lolololol. Vamos logo para de enrolar");
+
+        System.out.println("\n");
+
+        System.out.println("Após chegarem nos aposentos, Cleitin mostra a sala de reunião. Uma sala enorme, com uma grande mesa com "
+
+                + "\numa capacidade de até 20 lugares, uma janela com uma vista para "
+
+                + "\na cidade, do começo ao fim");
+
+        System.out.println("\n");
+
+       System.out.println(name + "Nossa pra que essa ignorância toda - se referindo a mesa");
+
+        System.out.println("\n");
+
+       System.out.println("Cleitin: Essa é a mesa de reunião dos líderes");
+
+        System.out.println("\n");
+
+        System.out.println(name + "E quantos líderes tem nessa cidade?");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Contando com você?");
+
+        System.out.println("\n");
+
+        System.out.println(name + "Sim!!!");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Só 1 ");
+
+        System.out.println("\n");
+
+        System.out.println(name + " Ué lolololol");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin:Mas pensa esse é só o começo, logo você não terá paciência para tanto falatório aqui");
+
+        System.out.println("\n");
+
+        System.out.println(name + ":Estou confiante. Me mostre a cidade, quero entender tud…");
+
+        System.out.println("\n");
+
+        System.out.println("?????: CLEITIN,CLEITIN,CLEITIN ESTÃO ATACANDO A NOSSA CIDADE!!!");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin:Calma, isso é impossível…Se fosse um ataque o meu sensor teria avisado");
+
+        System.out.println("\n");
+
+        System.out.println("?????Sério mesmo? Então me explique isso - Mostra pela janela para Cleitin, o ataque acontecendo");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Droga, vamos lá Zezin!");
+
+        System.out.println("\n");
+
+        System.out.println("????? :Deixa pra lá, não vai adiantar… é sempre a mesma coisa, já até foram embora."
+
+                + "\nAliás, quem é esse aí?");
+
+        System.out.println("\n");
+
+        System.out.println(name + "O-oi sou o novo Líder!");
+
+        System.out.println("\n");
+
+        System.out.println("????? :Duvido, estranho desse jeito… só vai afundar ainda mais a nossa cidade");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Acredite, ele tem essa capacidade");
+
+        System.out.println("\n");
+
+        System.out.println(name +"Já quero começar agir");        
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Gosto assim. E oque você quer fazer primeiro?");
+
+        System.out.println("\n");
+
+        System.out.println(name + "O jeito mais fácil para resolver isso é utilizando o método da Administração Clássica");
+
+        System.out.println("\n");
+
+        System.out.println("Cleitin: Qual opção voc e quer escolher? "); 
+
+        System.out.println("\n");
+
+        System.out.println("Opção 1-Reconstruir as fazendas"
+
+                + "\n(Ao escolher essa opção, voce tra-la dicipliana as pessoas de sua cidade e responsabilidade. Aqui é aplicadaa Adm. Classica)"
+
+                + "\n[+ Alimentos; + Energia; +Força]");
+
+        System.out.println("\n");
+
+        System.out.println("Opção 2- Reconstruir os muros"
+
+                + "\n(Ao escolher essa opção, vocw tra-la autoridade e ordem para sua cidade, alem de aumneto de sua defesa)"
+
+                + "\n[+Defea; -energia; +ordem]");
+
+          
+
+         
+
+            System.out.println("Deseja Entrar Nessa Jornada? [1/2]" );
+
+    
+
+    
+
+        
+
+    
+
+    }
+
+        Scanner s1 = new Scanner(System.in);
+
+        String lt1= s1.nextLine();
+
+
+        if(lt1.equals("1")) {
+
+        System.out.println("Ele morreu!");
+
+        }
+
+        else if(lt1.equals("2")) {
+
+            
+
+        System.out.println("gghhghhghgh");
+
+    
+
+     }
+
+    
+
+     }
+
+        
+
+    
+
+    
+
+            
+
+    }
+
+
+
